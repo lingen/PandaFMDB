84d7e9d5aa9e9a8b50dc6679d45715b72b2dc25c
lingen.liu <lingen.liu@gmail.com>
2016-03-24 09:32:43 +0800

    测试类完善

--- Example/Tests/OPFRepositoryTest.m
+++ Example/Tests/OPFRepositoryTest.m
@@ -12,6 +12,8 @@
 
 @interface OPFRepositoryTest : XCTestCase
 
+@property (nonatomic,strong) OPFRepository* repository;
+
 @end
 
 @implementation OPFRepositoryTest
@@ -19,29 +21,41 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSArray* tables = @[[Person class]];
+    NSString* dbPath = NULL;
+    _repository = [[OPFRepository alloc] initWith:dbPath tables:tables version:1];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    _repository = nil;
 }
 
 - (void)testExample {
-    
-    NSArray* tables = @[[Person class]];
-    
-    NSString* dbPath = NULL;
-    
-    OPFRepository* repository = [[OPFRepository alloc] initWith:dbPath tables:tables version:1];
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        BOOL tableExists = [repository syncQueryTableExists:@"person_"];
+        BOOL tableExists = [_repository syncQueryTableExists:@"person_"];
         
         XCTAssertTrue(tableExists);
     });
 
     [self waitForTimeInterval:4];
         
+}
+
+- (void)testThreads {
+    
+    for(int i=0;i<20;i++){
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+            BOOL tableExists = [_repository syncQueryTableExists:@"person_"];
+            
+            XCTAssertTrue(tableExists);
+        });
+    }
+
+    
+    [self waitForTimeInterval:10];
+    
 }
 
 - (void)waitForTimeInterval:(NSTimeInterval)delay
